04ac453eb4dc610687c57780770910ff44ef3c12
Ergin Denis <denis.ergin@vier.ai>
2022-02-09 10:11:55 +0100

    base setup for reducer (used to be then in UI)

--- src/App.tsx
+++ src/App.tsx
@@ -1,30 +1,10 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.scss';
 
-interface Properties {
-  name: string,
-}
-
-const App: React.FC<Properties> = ({
-  name,
-}) => {
+const App: React.FC = () => {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Hello {name}
-        </a>
-      </header>
+      Test
     </div>
   );
 };
